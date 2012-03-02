885252a293abe163d51a6b74ffecea3e7c217601
Yi Bo <phpatch@gmail.com>
2012-03-02 17:30:17 +0800

    upgrade mtputty and heidisql

--- install.bat
+++ install.bat
@@ -6,14 +6,14 @@ wget -P downloads/ http://nginx.org/download/nginx-1.1.12.zip
 wget -P downloads/ http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.20-win32.zip/from/http://mysql.stu.edu.tw/
 wget -P downloads/ http://windows.php.net/downloads/releases/php-5.3.10-nts-Win32-VC9-x86.zip
 wget -P downloads/ http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/3.4.7.1/phpMyAdmin-3.4.7.1-english.7z?r=http%3A%2F%2Fwww.phpmyadmin.net%2Fhome_page%2Fdownloads.php&ts=1321757623&use_mirror=nchc
-wget -P downloads/ http://heidisql.googlecode.com/files/HeidiSQL_6.0_Portable.zip
+wget -P downloads/ http://heidisql.googlecode.com/files/HeidiSQL_7.0_Portable.zip
 wget -P downloads/ http://download.sysinternals.com/Files/SysinternalsSuite.zip
 wget -P downloads/ http://msysgit.googlecode.com/files/PortableGit-1.7.9-preview20120201.7z
 wget -P downloads/ http://downloads.sourceforge.net/project/win32svn/1.6.17/svn-win32-1.6.17.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwin32svn%2Ffiles%2F1.6.17%2F&ts=1328605348&use_mirror=nchc
 wget -P downloads/ http://the.earth.li/~sgtatham/putty/latest/x86/putty.zip
 
 echo extracting...
-7z x downloads/HeidiSQL_6.0_Portable.zip -oHeidiSQL
+7z x downloads/HeidiSQL_7.0_Portable.zip -oHeidiSQL
 7z x downloads/PortableGit-1.7.9-preview20120201.7z -ogit
 7z x downloads/svn-win32-1.6.17.zip
 7z x downloads/putty.zip -obin
