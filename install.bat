c8660c2f7bf5f91536051dabdadb4482d1a3326c
Yi Bo <yibo.tm@gmail.com>
2012-04-04 14:03:21 +0800

    download only when not exist

--- install.bat
+++ install.bat
@@ -2,30 +2,60 @@
 title PHPToolChain Installer
 set path=%~dp0bin;%path%
 echo downloading...
-wget -P downloads/ http://nginx.org/download/nginx-1.1.12.zip
-wget -P downloads/ http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.20-win32.zip/from/http://mysql.stu.edu.tw/
-wget -P downloads/ http://windows.php.net/downloads/releases/php-5.3.10-nts-Win32-VC9-x86.zip
-wget -P downloads/ http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/3.4.7.1/phpMyAdmin-3.4.7.1-english.7z?r=http%3A%2F%2Fwww.phpmyadmin.net%2Fhome_page%2Fdownloads.php&ts=1321757623&use_mirror=nchc
-wget -P downloads/ http://heidisql.googlecode.com/files/HeidiSQL_7.0_Portable.zip
-wget -P downloads/ http://download.sysinternals.com/Files/SysinternalsSuite.zip
-wget -P downloads/ http://msysgit.googlecode.com/files/PortableGit-1.7.9-preview20120201.7z
-wget -P downloads/ http://downloads.sourceforge.net/project/win32svn/1.6.17/svn-win32-1.6.17.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwin32svn%2Ffiles%2F1.6.17%2F&ts=1328605348&use_mirror=nchc
-wget -P downloads/ http://the.earth.li/~sgtatham/putty/latest/x86/putty.zip
+rem SET SF_MIRROR=nchc
+SET SF_MIRROR=jaist
+SET NGINX_VERSION=1.1.12
+SET MYSQL_VERSION=5.5.22
+SET MYSQL_MIRROR=http://ftp.jaist.ac.jp/pub/mysql/
+SET PHPMYADMIN_VERSION=3.4.7.1
+SET PHP_VERSION=5.3.10
+SET GIT_VERSION=1.7.9
+SET GIT_RELEASE=preview20120201
+SET HEIDISQL_VERSION=7.0
+SET SVN_VERSION=1.6.17
 
+
+IF NOT EXIST downloads/nginx-%NGINX_VERSION%.zip (
+	wget -P downloads/ http://nginx.org/download/nginx-%NGINX_VERSION%.zip
+)
+IF NOT EXIST downloads/mysql-%MYSQL_VERSION%-win32.zip (
+	wget -P downloads/ http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-%MYSQL_VERSION%-win32.zip/from/%MYSQL_MIRROR%
+)
+IF NOT EXIST downloads/php-%PHP_VERSION%-nts-Win32-VC9-x86.zip (
+	wget -P downloads/ http://windows.php.net/downloads/releases/php-%PHP_VERSION%-nts-Win32-VC9-x86.zip
+)
+IF NOT EXIST downloads/phpMyAdmin-%PHPMYADMIN_VERSION%-english.7z (
+	wget -P downloads/ "http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/%PHPMYADMIN_VERSION%/phpMyAdmin-%PHPMYADMIN_VERSION%-english.7z?use_mirror=%SF_MIRROR%"
+)
+IF NOT EXIST downloads/HeidiSQL_%HEIDISQL_VERSION%_Portable.zip (
+	wget -P downloads/ http://heidisql.googlecode.com/files/HeidiSQL_%HEIDISQL_VERSION%_Portable.zip
+)
+IF NOT EXIST downloads/SysinternalsSuite.zip (
+	wget -P downloads/ http://download.sysinternals.com/files/SysinternalsSuite.zip
+)
+IF NOT EXIST downloads/PortableGit-%GIT_VERSION%-%GIT_RELEASE%.7z (
+	wget -P downloads/ http://msysgit.googlecode.com/files/PortableGit-%GIT_VERSION%-%GIT_RELEASE%.7z
+)
+IF NOT EXIST downloads/svn-win32-%SVN_VERSION%.zip (
+	wget -P downloads/ "http://downloads.sourceforge.net/project/win32svn/%SVN_VERSION%/svn-win32-%SVN_VERSION%.zip?use_mirror=%SF_MIRROR%"
+)
+IF NOT EXIST downloads/putty.zip (
+	wget -P downloads/ http://the.earth.li/~sgtatham/putty/latest/x86/putty.zip
+)
 echo extracting...
-7z x downloads/HeidiSQL_7.0_Portable.zip -oHeidiSQL
-7z x downloads/PortableGit-1.7.9-preview20120201.7z -ogit
-7z x downloads/svn-win32-1.6.17.zip
+7z x downloads/HeidiSQL_%HEIDISQL_VERSION%_Portable.zip -oHeidiSQL
+7z x downloads/PortableGit-%GIT_VERSION%-%GIT_RELEASE%.7z -ogit
+7z x downloads/svn-win32-%SVN_VERSION%.zip
 7z x downloads/putty.zip -obin
 7z x downloads/SysinternalsSuite.zip -oSysinternalsSuite
-7z x downloads/nginx-1.1.12.zip
-7z x downloads/php-5.3.10-nts-Win32-VC9-x86.zip -oPHP
-7z x downloads/phpMyAdmin-3.4.7.1-english.7z -owww\phpMyAdmin
-7z x downloads/mysql-5.5.20-win32.zip
+7z x downloads/nginx-%NGINX_VERSION%.zip
+7z x downloads/php-%PHP_VERSION%-nts-Win32-VC9-x86.zip -oPHP
+7z x downloads/phpMyAdmin-%PHPMYADMIN_VERSION%-english.7z -owww\phpMyAdmin
+7z x downloads/mysql-%MYSQL_VERSION%-win32.zip
 
-rename mysql-5.5.20-win32 MySQL
-rename svn-win32-1.6.17 svn
-rename nginx-1.1.12 nginx
+rename mysql-%MYSQL_VERSION%-win32 MySQL
+rename svn-win32-%SVN_VERSION% svn
+rename nginx-%NGINX_VERSION% nginx
 
 echo configuring
 copy/Y php_memcache.dll PHP\ext\
